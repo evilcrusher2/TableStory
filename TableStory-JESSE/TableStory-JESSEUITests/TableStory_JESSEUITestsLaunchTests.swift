732a6b238d530480c594801fcb35f52028211a53
evilcrusher2 <104800981+evilcrusher2@users.noreply.github.com>
2023-03-22 13:47:03 -0500

    Add files via upload
    start of file/app

---
+++ TableStory-JESSE/TableStory-JESSEUITests/TableStory_JESSEUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TableStory_JESSEUITestsLaunchTests.swift
+//  TableStory-JESSEUITests
+//
+//  Created by Williams, Jesse on 3/22/23.
+//
+
+import XCTest
+
+final class TableStory_JESSEUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
